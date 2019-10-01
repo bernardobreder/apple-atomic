8148a2b2ec3c788619bf420648cfaad19543479a
Bernardo Breder <bernardobreder@gmail.com>
2019-10-01 20:50:02 -0300

    .

--- Package.swift
+++ Package.swift
@@ -1,15 +1,22 @@
-//
-//  Package.swift
-//  AtomicValue
-//
-//
+// swift-tools-version:5.1
 
 import PackageDescription
 
 let package = Package(
-	name: "AtomicValue",
-	targets: [
-		Target(name: "AtomicValue", dependencies: []),
-	]
+    name: "AtomicValue",
+    products: [
+        .library(
+            name: "AtomicValue",
+            targets: ["AtomicValue"]),
+    ],
+    dependencies: [
+    ],
+    targets: [
+        .target(
+            name: "AtomicValue",
+            dependencies: []),
+        .testTarget(
+            name: "AtomicValueTests",
+            dependencies: []),
+    ]
 )
-
